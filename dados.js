a8d4c4e462261f27d8b24eea3066c937d327192e
MBarbara-lab <barbaramelissa.me@gmail.com>
2024-09-07 21:56:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil. Sua história inspira milhões de jovens a perseguirem seus sonhos.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris 2024. Sua dedicação e técnica impecável a tornaram um dos maiores nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Nascida em Guarulhos, a atleta conquistou o coração dos brasileiros com sua determinação e talento. Com um histórico de superação de lesões, Rebeca se tornou a primeira ginasta brasileira a conquistar uma medalha de ouro olímpica, além de outras diversas conquistas em competições nacionais e internacionais. Sua história inspira não apenas atletas, mas todos aqueles que buscam superar obstáculos e alcançar seus objetivos.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+]
